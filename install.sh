f4c362f27cfe4d1d287ca88800c19de2bf587747
Jairo Suarez <jasuarezpr@gmail.com>
2012-12-10 16:54:22 +0000

    deleting old unused .bashrc

--- install.sh
+++ install.sh
@@ -5,9 +5,4 @@ if [ -f "$HOME/.vimrc" ]; then
     echo "Backing up vimrc"
     cp $HOME/.vimrc $HOME/.vimrc.bak
 fi
-if [ -f "$HOME/.bashrc" ]; then
-    echo "Backing up bashrc"
-    cp $HOME/.bashrc $HOME/.bash.bak
-fi
 echo "source $DIR/.vimrc" > $HOME/.vimrc
-echo "source $DIR/.bashrc" > $HOME/.bashrc
